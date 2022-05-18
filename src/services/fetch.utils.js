fad6b64a77af31271d6b7927a5e27a0841cfe55e
Beau Elliott <belliott1516@gmail.com>
2022-05-18 11:26:47 -0700

    completed weather section

--- src/services/fetch.utils.js
+++ src/services/fetch.utils.js
@@ -9,7 +9,6 @@ export async function getPokemon(query){
 export async function getYelpData(query){
   const response = await fetch(`/.netlify/functions/yelp?location=${query}`);
   const data = await response.json();
-  console.log(data, 'fetch utils data');
   return data;
 }
 
